f93bdae7cae1700e95b50a3842d70cd4f9d9b182
Thomas de Groot <degroot12@gmail.com>
2021-02-05 16:56:35 +0100

    first iteration done

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require('mongoose')
+require('../configs/db.config.js')
+
+// retrieving the dronemodel
+let DroneModel = require('../models/Drone.model.js')
+
+const drones = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+  ];
+// putting drones inside the model.
+DroneModel.create(drones)
+  .then((result) => {
+    console.log('Drones are created', result)
+    mongoose.connection.close()
+  })
+  .catch(() => {
+    console.log('error with creating drones')
+  })
---
+++ models/Drone.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+
+let DroneSchema = new mongoose.Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+
+let DroneModel = mongoose.model('mydrone', DroneSchema)
+
+module.exports = DroneModel
