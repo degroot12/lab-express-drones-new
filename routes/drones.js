b6e6b1e506260b4d393b1c8e04e11b8c0726ad7c
Thomas de Groot <degroot12@gmail.com>
2021-02-05 18:18:52 +0100

    iteration 4 done

--- routes/drones.js
+++ routes/drones.js
@@ -45,11 +45,38 @@ router.post('/drones/create', (req, res, next) => {
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  let id = req.params.id
+  DroneModel.findById(id)
+    .then((drone) => {
+      res.render('drones/update-form.hbs', {drone})
+    })
+    .catch(() => {
+      console.log('error while finding drone')
+    })
+  
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  let id = req.params.id
+  const{myDrone, myPropellers, myMaxspeed} = req.body
+  
+  let updatedDrone = {
+    name: myDrone,
+    propellers: myPropellers,
+    maxSpeed: myMaxspeed
+  }
+
+  DroneModel.findByIdAndUpdate(id, updatedDrone)
+    .then(() => {
+      res.redirect('/drones')
+      console.log('updated succedfull')
+    })
+    .catch(() => {
+      console.log('error while editing the drone')
+      res.render('drones/:id/edit')
+    })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
